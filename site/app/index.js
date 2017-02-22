4edd249e5e32713f3ab38cd912d328b705f1fff0
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-21 20:13:13 -0600

    get basic webpack config up and running

---
+++ site/app/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+import React from 'react';
+
+import App from './app.js';
+console.log('apppp', app)
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplate = require('html-webpack-template');
+const path = require('path');
+
+const paths = {
+	app: path.join(__dirname, 'site', 'app'),
+	build: path.join(__dirname, 'site', 'build')
+}
+
+module.exports = {
+	devServer: {
+		inline: true,
+		port: process.env.PORT,
+		contentBase: paths.app
+	},
+	resolve: {
+    extensions: ['.js', '.jsx']
+  	},
+	entry: paths.app,
+	output: {
+		path: paths.build,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				include: paths.app
+			},
+			{
+				test: /\.css/,
+				loaders: ['style-loader', 'css-loader'],
+				include: paths.app
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: HtmlWebpackTemplate,
+			title: 'Band Page',
+        	appMountId: 'app',
+        	mobile: true,
+        	inject: false
+		})
+	]
+}
