184452136b59a9ea58e01eb1525939bb4469941b
Dillon Lundell <dillonjaylundell@gmail.com>
2017-03-10 21:25:55 -0600

    add a few tests to test out reducers( using action creators )

--- site/app/actions/song_actions.js
+++ site/app/actions/song_actions.js
@@ -1,6 +1,5 @@
 import { ADD_SONG } from './actions';
 
-
 export function addSong(song) {
 	return {
 		type: ADD_SONG,
---
+++ site/spec/reducers.spec.js
@@ -0,0 +1,20 @@
+/* jest */
+
+// Going to test action creators here as well. Good idea. I think so. 
+
+import reducers from '../app/reducers/index';
+import { selectSong } from '../app/actions/select_song_action';
+import { addSong } from '../app/actions/song_actions';
+
+
+test('SELECT_SONG', () => {
+  let state;
+   state = reducers({songs:[{artist:'JAY Z',genre:'rap',title:'big Pimpin'}],activeSong:null,data:null}, selectSong({artist:'JAY Z',genre:'rap',title:'big Pimpin'}));
+   expect(state).toEqual({songs:[{artist:'JAY Z',genre:'rap',title:'big Pimpin'}],activeSong:{artist:'JAY Z',genre:'rap',title:'big Pimpin'},data:null});
+});
+
+test('ADD_SONG', () => {
+  let state;
+  state = reducers({songs:[{artist:'JAY Z',genre:'rap',title:'big Pimpin'}],activeSong:null,data:null}, addSong({title:'saaahdude',artist:'dillon',genre:'rap'}));
+  expect(state).toEqual({songs:[{artist:'JAY Z',genre:'rap',title:'big Pimpin'},{title:'saaahdude',artist:'dillon',genre:'rap'}],activeSong:null,data:null});
+});
