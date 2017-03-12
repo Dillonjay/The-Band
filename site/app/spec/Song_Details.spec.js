e05fde73eb0cd0aceb22aec626b024d10a5565af
Dillon Lundell <dillonjaylundell@gmail.com>
2017-03-11 18:21:39 -0600

    create seperate snapshot for song Details when no song is desplayed

--- site/app/spec/Song_Details.spec.js
+++ site/app/spec/Song_Details.spec.js
@@ -4,9 +4,20 @@ import { shallowToJson } from 'enzyme-to-json';
 import { Provider } from 'react-redux';
 import store from '../store';
 import { Unwrapped as UnwrappedSongDetails } from '../containers/song_details';
+import selectSong from '../actions/select_song_action';
 
-test('SongDetails Snapshot Test', () => {
+test('SongDetails Snapshot Test With Song', () => {
 	const component = shallow(<UnwrappedSongDetails song={ {title: 'Bird', artist: 'Anderson Paak', genre: 'hiphop'} } /> );
 	const tree = shallowToJson(component);
 	expect(tree).toMatchSnapshot();
 })
+
+test('SongDetails Snapshot Test No Song', () => {
+	const component = shallow( <UnwrappedSongDetails /> );
+	const tree = shallowToJson( component );
+	expect(tree).toMatchSnapshot();
+})
+
+// test('Should Display Selected Song Details', () => {
+// 	const song = {title: 'Bird', artist: 'Anderson Paak', genre: 'hiphop'}
+// })
