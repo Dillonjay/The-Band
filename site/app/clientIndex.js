138a09a3958b9d155ddb62534880c040f309eb2b
Dillon Lundell <dillonjaylundell@gmail.com>
2017-03-13 13:53:42 -0500

    Create clientIndex.js as well as server.js

---
+++ site/app/clientIndex.js
@@ -0,0 +1,27 @@
+/* global document:false */
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Match } from 'react-router';
+import { Provider } from 'react-redux'
+import store from './store'
+import SongList from './containers/songs_list';
+import SongDetails from './containers/song_details';
+
+
+
+const App = React.createClass({
+    render () {
+        return (
+            <BrowserRouter>
+                <Provider store={store}>
+                    <div>
+                        <Match exactly pattern='/' component={ SongList } />
+                        <Match pattern='/hello' component={ SongDetails } />
+                    </div>
+                </Provider>
+            </BrowserRouter>
+        )
+    }
+})
+
+render(<App />, document.getElementById('app'))
